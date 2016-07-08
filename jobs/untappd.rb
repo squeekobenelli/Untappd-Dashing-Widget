b66b3f719de06be0654f3a8e846e778fc2bfe371
squeekobenelli <squeekobenelli@users.noreply.github.com>
2016-07-08 12:21:45 +0100

    Update untappd.rb

--- jobs/untappd.rb
+++ jobs/untappd.rb
@@ -1,12 +1,13 @@
 require "net/https"
 require "json"
 
-# Initialise Variables
+# CONFIG - Initialise Variables
 # API ClientID & Secret
 cliID = '<ENTER YOUR CLIENT ID HERE>'
 cliSec = '<ENTER YOUR CLIENT SECRET HERE>'
 # Untappd Username
 user = '<ENTER YOUR UNTAPPED USERNAME HERE>'
+
 # Uncomment whichever size of Badge to display
 #badgeimagesize = 'badge_image_sm'
 badgeimagesize = 'badge_image_md'
@@ -37,6 +38,8 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
                 latestname = result["response"]["user"]["recent_brews"]["items"][0]["beer"]["beer_name"]
                 latestrating = result["response"]["user"]["recent_brews"]["items"][0]["beer"]["auth_rating"]
                 latestbrewery = result["response"]["user"]["recent_brews"]["items"][0]["brewery"]["brewery_name"]
+                avatar = result["response"]["user"]["user_avatar"]
+                profile_url = result["response"]["user"]["untappd_url"]
                 send_update_1 = true
         end
 
@@ -54,6 +57,6 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
         end
 
         if send_update_1 && send_update_2
-                send_event('untappd', { untappd_uniquebeers: uniquebeers, untappd_badges: numbadges, untappd_latest_beer_label: latestlabel, untappd_latest_beer_name: latestname, untappd_latest_beer_myrating: latestrating, untappd_latest_beer_brewery: latestbrewery, untappd_latest_badge: latestbadge, untappd_latest_badge_image: latestbadgeimage })
+                send_event('untappd', { untappd_username: user, untappd_avatar: avatar, untappd_url: profile_url, untappd_uniquebeers: uniquebeers, untappd_badges: numbadges, untappd_latest_beer_label: latestlabel, untappd_latest_beer_name: latestname, untappd_latest_beer_myrating: latestrating, untappd_latest_beer_brewery: latestbrewery, untappd_latest_badge: latestbadge, untappd_latest_badge_image: latestbadgeimage })
         end
 end
